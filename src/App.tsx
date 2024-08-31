ab9c46d103144e20645d3637cc7d52a99985ab7c
Udeme98 <Darealudeme1@gmail.com>
2024-08-31 23:19:13 +0100

    my projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./Hero";
 import { BrowserRouter } from "react-router-dom";
+import Projects from "./Projects";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <BrowserRouter>
         <Header />
         <Hero />
+        <Projects />
       </BrowserRouter>
     </>
   );
---
+++ src/Projects.tsx
@@ -0,0 +1,16 @@
+const Projects = () => {
+  return (
+    <>
+      <section className="bg-slate-400">
+        <div className="container mx-auto text-center py-8">
+          <h2 className="text-3xl text-red-900 font-semibold mb-4">
+            My Projects
+          </h2>
+          <hr className="text-red-900" />
+        </div>
+        <div></div>
+      </section>
+    </>
+  );
+};
+export default Projects;
