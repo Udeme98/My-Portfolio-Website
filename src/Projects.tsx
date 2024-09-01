3a56446a5ae053a5beac6aa3a856f1c92be10ffd
Udeme98 <Darealudeme1@gmail.com>
2024-09-01 10:36:21 +0100

    my projects section

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,14 +1,23 @@
 const Projects = () => {
   return (
     <>
-      <section className="bg-slate-400">
+      <section className="bg-slate-800">
         <div className="container mx-auto text-center py-8">
-          <h2 className="text-3xl text-red-900 font-semibold mb-4">
+          <h2 className="text-2xl uppercase text-red-600 font-semibold mb-4">
             My Projects
           </h2>
-          <hr className="text-red-900" />
         </div>
-        <div></div>
+        <div className="container mx-auto grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center">
+          <div className=" w-[400px] h-[400px] bg-red-600"></div>
+          <div className=" w-[400px] h-[400px] bg-blue-600"></div>
+          <div className=" w-[400px] h-[400px] bg-green-600"></div>
+
+          {/* <div className="h-[400px] w-[400px] bg-yellow-600"></div> */}
+          {/* <div className="bg-red-600 w-full aspect-w-1 aspect-h-1"></div>
+          <div className="bg-blue-600 w-full aspect-w-1 aspect-h-1"></div>
+          <div className="bg-green-600 w-full aspect-w-1 aspect-h-1"></div>
+          <div className="bg-yellow-600 w-full aspect-w-1 aspect-h-1"></div> */}
+        </div>
       </section>
     </>
   );
