47793f6da378e03b3339472962c84cd3fd40f366
Udeme98 <Darealudeme1@gmail.com>
2024-09-02 17:07:10 +0100

    my projects section - Project card component

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -2,8 +2,8 @@ const Projects = () => {
   return (
     <>
       <section className="bg-slate-800">
-        <div className="container mx-auto text-center py-8">
-          <h2 className="text-2xl uppercase text-red-600 font-semibold mb-4">
+        <div className="container mx-auto  text-center py-8">
+          <h2 className="text-2xl uppercase text-red-600 font-semibold mb-4 border-b-2 border-red-600  inline-block">
             My Projects
           </h2>
         </div>
@@ -15,15 +15,15 @@ const Projects = () => {
               alt="nike-thumbnail"
               className="w-full object-cover"
             />
-            <div className="mt-3 font-montserrat text-slate-300">
+            <div className="mt-3 font-mono text-slate-300">
               <h2 className="mb-2">Nike Landing Page</h2>
               <h2 className="text-xs">HTML/CSS, TailwindCSS</h2>
             </div>
             <div className="flex gap-3 mt-4">
-              <button className="bg-red-600 hover:bg-red-500 py-1 px-1 text-gray-300 rounded-sm">
+              <button className="bg-red-600 hover:bg-red-500 py-1 px-1 text-gray-300 rounded-md text-xs">
                 GitHub
               </button>
-              <button className="bg-red-600 hover:bg-red-500 py-1 px-1 text-gray-300 rounded-sm">
+              <button className="bg-red-600 hover:bg-red-500 py-1 px-1 text-gray-300 rounded-md text-xs">
                 Live Demo
               </button>
             </div>
