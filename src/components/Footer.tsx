7b55654b399af2dba0f533424c313c8d313ed55a
Udeme98 <Darealudeme1@gmail.com>
2024-09-23 10:03:01 +0100

    Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,8 @@
 const Footer = () => {
-  return <footer className="bg-slate-800 text-red-600">Footer</footer>;
+  return (
+    <footer className="bg-slate-800 text-red-600">
+      <div className="container mx-auto px-4">footer</div>
+    </footer>
+  );
 };
 export default Footer;
