fc395ed9559a1c041e95eef8e44e1a98dbe0675a
Udeme98 <Darealudeme1@gmail.com>
2024-09-23 10:56:38 +0100

    Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,8 @@
 const Footer = () => {
   return (
     <footer className="bg-slate-800 text-white py-6">
-      <div className="container mx-auto">
-        <p className="text-lg mb-4">Connect with me:</p>
-        <div className="flex justify-center space-x-6">
+      <div className="container mx-auto flex flex-col items-center justify-center">
+        <div className="flex gap-5">
           <a
             href="https://www.github.com/Udeme98"
             target="_blank"
