d108a8d4b2588e867ff2ed01063f826cd7873b35
Udeme98 <Darealudeme1@gmail.com>
2024-09-23 10:27:37 +0100

    Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,35 @@
 const Footer = () => {
   return (
-    <footer className="bg-slate-800 text-red-600">
-      <div className="container mx-auto px-4">footer</div>
+    <footer className="bg-slate-800 text-white py-6">
+      <div className="container mx-auto">
+        <p className="text-lg mb-4">Connect with me:</p>
+        <div className="flex justify-center space-x-6">
+          <a
+            href="https://www.github.com/Udeme98"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="https://www.linkedIn.com/in/darealudeme1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://www.twitter.com/Darealudeme1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+        </div>
+        <p className="mt-4 text-sm">
+          &copy;{new Date().getFullYear()} Udeme Udeme. All Rights Reserved.
+        </p>
+      </div>
     </footer>
   );
 };
